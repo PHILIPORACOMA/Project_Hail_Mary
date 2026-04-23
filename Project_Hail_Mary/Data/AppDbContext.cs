5eae84f28556b6e22b1ad5f72b1352faa764c579
epica <epicarusracoma@gmail.com>
2026-04-23 09:33:30 +0800

    Add project files.

---
+++ Project_Hail_Mary/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_Hail_Mary.Models;
+using System;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Users> User { get; set; }
+}
---
+++ Project_Hail_Mary/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+namespace Project_Hail_Mary.Models
+{
+    public class Users 
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [EmailAddress(ErrorMessage = "Invalid email format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [StringLength(30, MinimumLength = 8)]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
