8cca65272f3026587b8a487ccb64899a077fa9d6
epica <epicarusracoma@gmail.com>
2026-04-29 21:17:02 +0800

    shop and home page updates

---
+++ Project_Hail_Mary/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Hail_Mary.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Shop()
+        {
+            return View();
+        }
+    }
+}
